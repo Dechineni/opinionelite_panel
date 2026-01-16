815e7a5fc568eb07371e1d0e0b4059f7b4df7570
Dechineni <rajdechineny@gmail.com>
2026-01-16 11:55:55 +0530

    profile ui corrections fix

--- UI/index.php
+++ UI/index.php
@@ -40,7 +40,7 @@ include('header.php');
     <div class="swiper-slide">
       <div class="card profile-card" data-profile="Work profile" style="cursor:pointer;">
         <img src="images/busin.png" alt="Icon 3" class="icon">
-        <h5>Business & Employment Profile</h5>
+        <h5>Professional Profile</h5>
         <h2>$2.00</h2>
         <!-- <p>7 MIN</p> -->
       </div>
@@ -49,7 +49,7 @@ include('header.php');
     <div class="swiper-slide">
       <div class="card profile-card" data-profile="Personal Profile" style="cursor:pointer;">
         <img src="images/enter.png" alt="Icon 4" class="icon">
-        <h5>Entertainment (TV/Film) Profile</h5>
+        <h5>Entertainment Profile</h5>
         <h2>$2.00</h2>
         <!-- <p>8 MIN</p> -->
       </div>
