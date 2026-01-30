afbf1b983c81b283ab5ad5c8df2041c09116f025
Dechineni <rajdechineny@gmail.com>
2026-01-30 23:35:17 +0530

    re added the debugging code

--- UI/get_profile_answers.php
+++ UI/get_profile_answers.php
@@ -1,5 +1,7 @@
 <?php
-
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
 // UI/get_profile_answers.php
 // Public endpoint (token protected) that returns OP Panel profile answers for a given user.
 
